1e7ff9bc7ac16fc9793058d9f6696cb51d49d0b7
Madi Assil <madiasunik@gmail.com>
2022-08-25 18:06:35 +0600

    Module 4.1 Task #1 first commit

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalTruePrinter {
     public static void main(String[] args) {
+        boolean booTrue = true;
+        if (booTrue == true) {
+            System.out.println(booTrue);
+        } else {
+            System.out.println("false");
+        }
     }
 }
